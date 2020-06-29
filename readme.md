09e45fb27f9664df8b159e861f0cb7efc3bcf44f
Rajesh Kotari <rkotari@centos8.lan>
2020-06-28 22:07:45 -0700

    writing producers and consumers

---
+++ readme.md
@@ -0,0 +1,24 @@
+To start rabbitmq on minikube, the following components are installed
+1) minikube
+2) kubectl
+3) kustomize
+
+minikube is started with the following command
+minikube start --driver=virtualbox
+when tried to start minikube with default driver docker ran into issues in download in images from docker so reverted to virtual box
+
+once the minikube is started run two additional commands to allow port forwarding
+
+1) kubectl port-forward service/rabbitmq 8080:15672   (This would allow to access rabbitmq on browser at http://localhost:8080)
+
+2) kubectl port-forward service/rabbitmq 5672:5672  (This is to submit/process messages to/from the queue)
+
+Followed the following article to setup rpc_client.py and rpc_server.py
+
+https://www.rabbitmq.com/tutorials/tutorial-six-python.html
+
+Created conda environment and pip installed pika 
+
+To start server python server/rpc_server.py
+
+To submit a request python client_rpc_client.py
---
+++ server/rpc_server.py
@@ -0,0 +1,45 @@
+import pika
+import random
+import time
+
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host='localhost'))
+
+channel = connection.channel()
+
+channel.queue_declare(queue='rpc_queue')
+
+
+def fib(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n - 1) + fib(n - 2)
+
+
+def on_request(ch, method, props, body):
+    n = int(body)
+
+    print(" [.] fib(%s)" % n)
+    # print(props.correlation_id)
+    rdm = random.randint(1,30)
+    print("correlation id %s sleeping for %s", props.correlation_id, rdm)
+    time.sleep(rdm)
+
+    response = fib(n)
+
+    ch.basic_publish(exchange='',
+                     routing_key=props.reply_to,
+                     properties=pika.BasicProperties(correlation_id = \
+                                                         props.correlation_id),
+                     body=str(response))
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(queue='rpc_queue', on_message_callback=on_request)
+
+print(" [x] Awaiting RPC requests")
+channel.start_consuming()
